9603403500b1734c7a88f337969f6272cd8279f5
anonymous anonymous <kate.reis@kellogg.northwestern.edu>
2022-02-21 01:17:48 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,15 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :seller_id, :integer
+  attribute :buyer_id, :integer
+  attribute :name, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
